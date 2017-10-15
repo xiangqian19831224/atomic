4e5b6705efec7b791abf20b2031ea2fb605fe407
Administrator <admin@example.com>
2017-10-15 09:12:05 +0800

    volatile

--- src/main/java/com/test/volatileLearn/VolatileCounter.java
+++ src/main/java/com/test/volatileLearn/VolatileCounter.java
@@ -1,9 +1,9 @@
 package com.test.volatileLearn;
 
 public class VolatileCounter {
-	public static volatile int count = 0;
+	public  volatile int count = 0;
 
-	public static void inc() {
+	public  void inc() {
 		//这里延迟1毫秒，使得结果明显
 		try {
 			Thread.sleep(1);
@@ -14,11 +14,12 @@ public class VolatileCounter {
 	}
 
 	public static void main(String[] args) {
+		final VolatileCounter volatileCounter = new VolatileCounter();
 		//同时启动1000个线程，去进行i++计算，看看实际结果
 		for (int i = 0; i < 1000; i++) {
 			new Thread(new Runnable() {
 				public void run() {
-					VolatileCounter.inc();
+					volatileCounter.inc();
 				}
 			}).start();
 		}
@@ -30,6 +31,6 @@ public class VolatileCounter {
 		}
 
 		//这里每次运行的值都有可能不同,可能为1000
-		System.out.println("运行结果:Counter.count=" + VolatileCounter.count);
+		System.out.println("运行结果:Counter.count=" + volatileCounter.count);
 	}
 }
